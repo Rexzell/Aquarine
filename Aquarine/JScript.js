73319ce6739e824c90ecbeda707e91f0e55f7ca0
John Rexzell G. Alip <62383423+Rexzell@users.noreply.github.com>
2026-03-10 10:58:28 +0800

    Add files via upload

--- Aquarine/JScript.js
+++ Aquarine/JScript.js
@@ -1,27 +1,28 @@
-//Navbar
 const navSlide = () => {
 	const burger = document.querySelector(".burger");
 	const navbar = document.querySelector(".navbar");
 	const navLinks = document.querySelectorAll(".navbar li");
 	
 	burger.addEventListener("click", () => {
-		//Toggle Map
 		navbar.classList.toggle("nav-active");
 		
-		//Animate Links
+		// Animate Links
 		navLinks.forEach((link, index) => {
 			if(link.style.animation){
 				link.style.animation = "";
 			}
 			else{
-				link.style.animation = link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
 			}
 		});
 		
-		//Burger Animation
 		burger.classList.toggle("toggle");
 	});
 }
 
 navSlide();
 
+window.addEventListener("load", function(){
+	const loader = document.querySelector(".loader-wrapper");
+	loader.style.display = "none";
+});
